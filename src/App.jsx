a6dc32c0f1491c4774d0eae4b0e253188651dbda
DiegoreNshore <diego.gutierrez@improving.com>
2023-04-19 15:29:23 -0600

    Miraje .js Implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Card, Movie } from "@mentor-io/npm-library-class";
 
-import { Card, Movie } from "@mentor-io/npm-ui-library";
+import "./index.scss";
 
 const App = () => (
 <Card title="Card en Home">
   <div>Name: Home Page</div>
-  <Movie
-    label="4DX"
-    movieTitle="Preventa"
-    src="https://static.cinepolis.com/img/peliculas/41041/1/1/41041.jpg"
-    variant="4dx"
-  />
-  <Movie
-    label="Promociones"
-    movieTitle="Movie 01"
-    src="https://static.cinepolis.com/img/peliculas/41818/1/1/41818.jpg"
-  />
   <div>Language: JavaScript</div>
+  <div className="movie-grid">
+    <Movie
+      label="4DX"
+      movieTitle="Preventa"
+      src="https://static.cinepolis.com/img/peliculas/41041/1/1/41041.jpg"
+      variant="4dx"
+    />
+    <Movie
+      label="Promociones"
+      movieTitle="Movie 01"
+      src="https://static.cinepolis.com/img/peliculas/41818/1/1/41818.jpg"
+    />
+  </div>
 </Card>
 );
 
